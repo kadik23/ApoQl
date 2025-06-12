f7b2a6f5ac1cfd79040ee94e84a36b896c7fc6b1
kadik23 <kadiksalah03@gmail.com>
2025-06-12 19:52:59 +0100

    integrate graphql with mongoose

--- server/index.js
+++ server/index.js
@@ -3,8 +3,22 @@ const { graphqlHTTP } = require('express-graphql');
 const port = 4000;
 const app = express();
 const schema = require('./schema/schema')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/graphql-ninja', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to MongoDB database');
+})
+.catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
+
 app.use('/graphql', graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
 }));
 
 app.listen(port, () => {
